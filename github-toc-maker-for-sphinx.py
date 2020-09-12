377ecba08d73cc296e887d65ef2b89e397b6fee7
weylin0714 <1424158077@qq.com>
2020-09-12 15:48:48 +0800

    update

---
+++ github-toc-maker-for-sphinx.py
@@ -0,0 +1,64 @@
+import os
+import re
+from glob import glob
+
+pwd = os.getcwd()
+all_chapters_path = glob(pwd + "/source/ch0*")
+
+
+def get_chapter_name(file):
+    with open(file, 'r', encoding="utf-8") as f:
+        f.readline()
+        chapter_name = f.readline().strip()
+
+    return chapter_name
+
+def get_title(file):
+    with open(file, 'r', encoding="utf-8") as f:
+        first_line = f.readline()
+
+    if first_line.startswith("#"):
+        return first_line[1:].strip()
+
+def generate_mapping():
+    mapping = dict.fromkeys([os.path.basename(chapter_path) for chapter_path in all_chapters_path])
+    for key in mapping.keys():
+        chapter_file = os.path.join(pwd, "source", "chapters", key + ".rst")
+        mapping[key] = get_chapter_name(chapter_file)
+
+    return mapping
+
+def get_toc_info():
+    toc = {}
+    for dir_path in all_chapters_path:
+        dir_name = os.path.basename(dir_path)
+
+        chapter_toc = {}
+        files = glob(dir_path + "/*.md")
+        for file in files:
+            file_name = os.path.basename(file)
+            section = int(re.findall(r"ch\d{2}_(\d{2}).md", file_name)[0])
+            md_path = "https://effectivepython.readthedocs.io/zh_CN/latest/" + dir_name + "/" + file_name.replace("md", "html")
+            title = get_title(file)
+            if not title:
+                continue
+            chapter_toc[section] = (title, md_path)
+
+        toc[dir_name] = chapter_toc
+    return toc
+
+def print_md_toc(toc_info, mapping):
+    for chapter in sorted(toc_info.items(), key=lambda item: item[0]):
+        posts = chapter[1]
+        chapter_name = mapping[chapter[0]]
+        print(f"- **{chapter_name}**")
+        for post in sorted(posts.items(), key=lambda item:item[0]):
+            print("  ", f"* [{post[1][0]}]({post[1][1]})")
+
+def main():
+    mapping = generate_mapping()
+    toc_info = get_toc_info()
+    print_md_toc(toc_info, mapping)
+
+if __name__ == '__main__':
+    main()
